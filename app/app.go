1778afe82aed2f1d022607bb57dab774c42dc853
Asher Buk <2asherbuk@gmail.com>
2025-10-28 02:48:54 +0200

    feat: add minimal skeleton code for wails dev

--- app/app.go
+++ app/app.go
@@ -2,4 +2,14 @@
 // SPDX-License-Identifier: Apache-2.0
 // https://github.com/AshBuk/FingerGo
 
+package app
 
+import "context"
+
+type App struct{}
+
+func New() *App { return &App{} }
+
+func (a *App) Startup(ctx context.Context) {}
+
+func (a *App) Shutdown(ctx context.Context) {}
--- internal/models/text.go
+++ internal/models/text.go
@@ -2,4 +2,4 @@
 // SPDX-License-Identifier: Apache-2.0
 // https://github.com/AshBuk/FingerGo
 
-
+package models
