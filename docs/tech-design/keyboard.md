df933f22c404da698e0058222b957a8e38fc9cbd
Asher Buk <2asherbuk@gmail.com>
2025-11-08 23:07:07 +0200

    correct finger mapping to match touch typing standards

--- docs/tech-design/keyboard.md
+++ docs/tech-design/keyboard.md
@@ -15,18 +15,20 @@ SPDX-License-Identifier: Apache-2.0
 #### Finger Mapping (Color-coded)
 
 Left Hand:
-- Pinky:    Q A Z 
-- Ring:     W S X 
-- Middle:   E D C 
-- Index:    R F V, T G B 
+- Pinky:    Q A Z ` 1 (+ Shift, Tab, Caps Lock) (color: red)
+- Ring:     W S X 2 (color: orange)
+- Middle:   E D C 3 (color: yellow)
+- Index:    R F V, T G B (4 5) (color: green)
 
 Right Hand:
-- Index:    Y H N, U J M (color: blue)
-- Middle:   I K , (color: indigo)
-- Ring:     O L . (color: violet)
-- Pinky:    P ; / (color: pink)
+- Index:    Y H N, U J M (6 7) (color: blue)
+- Middle:   I K , (8) (color: indigo)
+- Ring:     O L . (9) (color: violet)
+- Pinky:    P ; / [ ] ' \ - = (0) (+ Shift, Enter, Backspace) (color: pink)
 
 Thumbs: Space
+
+Note: Number row and symbols follow standard touch typing assignments.
 
 **Color for each current key - Pastel Blue**
 
--- frontend/src/js/layouts/en-qwerty.js
+++ frontend/src/js/layouts/en-qwerty.js
@@ -79,14 +79,19 @@
         ],
 
         fingerMap: {
+            // Left hand
             '`':'left-pinky','1':'left-pinky','q':'left-pinky','a':'left-pinky','z':'left-pinky','Tab':'left-pinky','CapsLock':'left-pinky','Shift':'left-pinky',
             '2':'left-ring','w':'left-ring','s':'left-ring','x':'left-ring',
             '3':'left-middle','e':'left-middle','d':'left-middle','c':'left-middle',
-            '4':'left-index','r':'left-index','f':'left-index','v':'left-index','5':'left-index','t':'left-index','g':'left-index','b':'left-index',
-            '6':'right-index','y':'right-index','h':'right-index','n':'right-index','7':'right-index','u':'right-index','j':'right-index','m':'right-index',
+            '4':'left-index','5':'left-index','r':'left-index','t':'left-index','f':'left-index','g':'left-index','v':'left-index','b':'left-index',
+
+            // Right hand
+            '6':'right-index','7':'right-index','y':'right-index','u':'right-index','h':'right-index','j':'right-index','n':'right-index','m':'right-index',
             '8':'right-middle','i':'right-middle','k':'right-middle',',':'right-middle',
             '9':'right-ring','o':'right-ring','l':'right-ring','.':'right-ring',
-            '0':'right-pinky','p':'right-pinky',';':'right-pinky','/':'right-pinky','[':'right-pinky',']':'right-pinky','\\':'right-pinky','-':'right-pinky','=':'right-pinky','Backspace':'right-pinky','Enter':'right-pinky',"'":'right-pinky',
+            '0':'right-pinky','-':'right-pinky','=':'right-pinky','p':'right-pinky',';':'right-pinky','/':'right-pinky','[':'right-pinky',']':'right-pinky','\\':'right-pinky','Backspace':'right-pinky','Enter':'right-pinky',"'":'right-pinky',
+
+            // Thumbs
             ' ':'thumb'
         }
     };
